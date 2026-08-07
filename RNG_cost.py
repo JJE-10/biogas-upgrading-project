49e46ad764b304a998e697ecb2ed181c7c11379c
Jesse Johnson <jjohnson316@uiowa.edu>
2026-08-07 17:23:13 -0500

    redoing calculations regarding biogas production and rng flow

--- RNG_cost.py
+++ RNG_cost.py
@@ -7,11 +7,11 @@ from scipy import stats
 MMBTU_TO_MJ   = 1055.06    # 1 MMBtu = 1,055.06 MJ 
 """ Conversion factor for MMBtu to MJ i.e 1 MMBtu = 1,055.06 MJ """
 
-MJ_PER_KG_CH4 = 55       # 1 kg CH4 = 55 MJ
-""" An assumption of 1 kg CH4 = 55 MJ, which is obtained from El Abbadi et al., preprint . """
+MJ_PER_KG_CH4 = 55.6       # 1 kg CH4 = 55.6 MJ
+""" An assumption of 1 kg CH4 = 55.6 MJ, which is obtained from El Abbadi et al., preprint . """
 
 # Loading flow data in m3/d from ad_data.csv 
-df = pd.read_csv('ad_data.csv')
+df = pd.read_csv(r'C:\Users\jjohnson316\OneDrive - University of Iowa\Research\codes\clean-data\ad_data.csv')
 flow_data = df['flow_m3_per_day'].values
 #print(flow_data)
 
@@ -26,10 +26,12 @@ biogas_gen = cal_biogas_gen(flow_data)
 print(biogas_gen)
 
 
-""" To calculate RNG cost which is cited from Parker et al., 2017, RNG flow was calculated in MMBtu/hr
- using the biogas generation in kgCH4/hr, the assumption of 1 kg CH4 = 55 MJ and the conversion factor of 1 MMBtu = 1,055.06 MJ."""
+""" To calculate RNG cost which is cited from Parker et al., 2017, RNG flow needs to be calculated in MMBtu/hr.
+With the upgrading technology having an efficiency of 90%, the RNG flow can be calculated 
+as rng-flow = 0.9*biogas production.
+ using the biogas generation in kgCH4/hr, the assumption of 1 kg CH4 = 55.6 MJ and the conversion factor of 1 MMBtu = 1,055.06 MJ."""
 
-rng_flow = biogas_gen * 1/MMBTU_TO_MJ * MJ_PER_KG_CH4
+biogas_prod = biogas_gen * 1/MMBTU_TO_MJ * MJ_PER_KG_CH4
 print(rng_flow)
 
 def biogas_upgrade_pipe(rng_flow):
