41a812fe1148b2a3f69a7e4ea3143c3e3147740c
Jesse Johnson <johnsonjesse36@gmail.com>
2026-08-10 02:06:37 -0500

    Just started working energy consumption of facilites

--- RNG_cost.py
+++ RNG_cost.py
@@ -41,9 +41,13 @@ rng_flow = 0.9*biogas_prod #this is the rng flowrate in mmBtu/h after upgrading
 """Assuming a membrane technology and a downtime of 3%(Patterson et al., 2011), the membrane technology 
 operates for  8497.2h per year."""
 
-#Calculating rng_flow1 in mmBtu/yr
-rng_flow1 = 8497.2*rng_flow
-#print(rng_flow1)
+#Calculating rng_flow1 in mmBtu/yr for membrane technology
+# opt is the technology operating time
+def rng_flow_cal(opt):
+ return opt*rng_flow
+
+rng_flow1 = rng_flow_cal(8497.2)
+#print("RNG flow for membrane technology(mmBtu/yr) = ", rng_flow1)
 
 """Calculating the capital cost for upgrading biogas for pipeline injection alongside it's annual o&m cost
 using cost relation from Parker etal.,2017"""
@@ -72,14 +76,27 @@ A unit capital cost in $/(mmBtu/yr) is also needed to annualize the capital cost
 
 # Calculting CRF 
 def crf_cal(t,wacc):
-   crf = wacc/(1-(1+wacc)**-t)
-t = 10
-wacc = 0.1
-crf = wacc/(1-(1+wacc)**-t)
-#print("CRF = ",crf)
+   return wacc/(1-(1+wacc)**-t)
 
-# Calculating unit capital cost (uncc)
-uncc = rng_cost/rng_flow1
-print("Unit Capital Cost($/mmBtu/yr) = ",uncc)
+crf1 = crf_cal(10,0.1)
+print("CRF for membrane technology = ",crf1)
+
+# Calculating unit capital cost (uncc) for membrane technology
+uncc1 = rng_cost/rng_flow1
+print("Unit Capital Cost for membrane technology($/mmBtu/yr) = ",uncc1)
+
+# Calculating annualized capital cost for membrane technology
+acc1 = crf1*uncc1
+print("Annualized Capital Cost for membrane technology($/mmBtu)", acc1)
+
+# Calculating annualized O&M cost for membrane technology
+aomc1 = o_m_cost/rng_flow1
+print("Annualized O&M Cost for membrane technology($/mmBtu)", aomc1)
 
 
+"""                               Energy consumption of technologies                                       
+For membrane technology, it's energy consumption for upgrading biogas is 0.3kWh/m3(Makaruk etal., 2010).
+To calculate the energy used by each facility assuming they all use membrane technology, biogas prodution in kgCH4/h
+needs to be converted to m3/h. Assuming biogas at all facilities is been produced at 25 degrees celcius and 1atm,
+then biogas HHV at these conditions is 890.3 KJ/moleCH4(NREL,2010).
+"""
